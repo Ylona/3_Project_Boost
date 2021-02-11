3b1d3adf35c5b4103dfc6b89e71fb72797355c18
ylonaMeeuwenberg <meeuwenberg.ylona@gmail.com>
2021-02-11 17:51:48 +0100

    Add audio while trusting

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -7,10 +7,12 @@ public class Rocket : MonoBehaviour
 {
 
     Rigidbody rigidbody;
+    AudioSource audio;
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        audio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -21,18 +23,28 @@ public class Rocket : MonoBehaviour
 
     private void ProcessInput()
     {
+
         if (Input.GetKey(KeyCode.Space))
         {
+            if (!audio.isPlaying)
+            {
+                audio.Play();
+            }
+            
             rigidbody.AddRelativeForce(Vector3.up);
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            audio.Stop();
         }
         
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(-Vector3.forward);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(Vector3.forward);
 
         }
     }
